8b067dee5c56397289ca4aace44233e12f7e61df
YQS <41525397+TomStone76@users.noreply.github.com>
2018-11-21 23:40:44 -0500

    commit

--- models/burger.js
+++ models/burger.js
@@ -1,30 +1,3 @@
-// var orm = require("../config/orm.js");
-
-// var burger = {
-//     all: function (cb) {
-//         orm.all("burgers", function (res) {
-//             cb(res);
-//         });
-//     },
-//     create: function (cols, vals, cb) {
-//         orm.insertOne("burgers", cols, vals, function (res) {
-//             cb(res);
-//         });
-//     },
-//     update: function (objColVals, condition, cb) {
-//         orm.update("burgers", objColVals, condition, function (res) {
-//             cb(res);
-//         });
-//     },
-//     delete: function(condition, cb) {
-//         orm.delete("burgers", condition, function(res) {
-//             cb(res);
-//         });
-//     }
-// };
-
-// module.exports = burger;
-
 module.exports = function(sequelize, DataTypes) {
     var burgers = sequelize.define("burgers", {
         burger_name: DataTypes.STRING,
